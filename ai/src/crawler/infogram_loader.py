3885da3994e854124e9a470a4a92663703c2c7eb
Rohit Priyadarshi <rohit@rishkan.com>
2024-04-30 16:54:04 -0700

    Added html dump

--- ai/src/crawler/infogram_loader.py
+++ ai/src/crawler/infogram_loader.py
@@ -1,10 +1,13 @@
 # https://stackoverflow.com/questions/63700789/how-to-extract-data-from-graph-from-a-web-page
+# https://www.freeformatter.com/html-validator.html
 import requests
 from bs4 import BeautifulSoup
 import re
 import json
 import pprint
 import param
+import pandas as pd
+
 
 class InfogramLoader(param.Parameterized):
     urls = param.List(doc="Urls to process")
@@ -13,6 +16,8 @@ class InfogramLoader(param.Parameterized):
         super(InfogramLoader, self).__init__( **params)
 
         self.data = {}
+        self.tab = 0
+        self.tab_size = 2
 
     def extract_tables(self, infogram):
         infogram_url = f'https://e.infogram.com/{infogram["data-id"]}'
@@ -56,6 +61,98 @@ class InfogramLoader(param.Parameterized):
                 infogram_data[sheet] = cols
         self.data[infogram_url] = infogram_data
 
+# <!DOCTYPE html>
+# <head>
+#   <meta charset="UTF-8">
+#   <title>Title of the document</title>
+# </head>
+# <table>
+#   <caption>Concerts</caption>
+#   <tr>
+#     <th>Company</th>
+#     <th>Contact</th>
+#     <th>Country</th>
+#   </tr>
+#   <tr>
+#     <td>Alfreds Futterkiste</td>
+#     <td>Maria Anders</td>
+#     <td>Germany</td>
+#   </tr>
+#   <tr>
+#     <td>Centro comercial Moctezuma</td>
+#     <td>Francisco Chang</td>
+#     <td>Mexico</td>
+#   </tr>
+# </table>
+
+    def get_spaces(self):
+        return ' ' * self.tab
+    
+    def get_sep(self):
+        return '\n' + self.get_spaces()
+    
+    def get_tab(self):
+        return self.get_sep()
+
+    def get_tabup(self):
+        self.tab += self.tab_size
+        return self.get_sep()
+    
+    def get_tabdn(self):
+        self.tab -= self.tab_size
+        return self.get_sep()
+    
+    def reset_tab(self):
+        self.tab = 0
+
+    def set_tab_size(self, size: int):
+        self.tab_size = size
+
+    def to_html(self):
+        self.set_tab_size(2)
+        html = f'<!DOCTYPE html>'
+        html = self.get_tab().join([html, f'<head>'])
+        html = self.get_tabup().join([html, f'<meta charset="UTF-8">'])
+        html = self.get_tab().join([html, f'<title>Infogram Extraction</title>'])
+        self.get_tabdn()
+        html = self.get_tabdn().join([html, f'</head>'])
+
+        for key0, value0 in self.data.items():
+            html = self.get_tabup().join([html, f'<table>'])
+            html = self.get_tabup().join([html, f'<caption>{key0}</caption>'])
+            self.get_tabdn()
+            
+            html = self.get_tabup().join([html, f'<tr>'])
+            html = self.get_tabup().join([html, f'<td>'])
+            for key1, value1 in value0.items():
+                html = self.get_tabup().join([html, f'<table>'])
+                html = self.get_tabup().join([html, f'<caption>{key1}</caption>'])
+                self.get_tabdn()
+
+                html = self.get_tabup().join([html, f'<thead>'])
+                html = self.get_tabup().join([html, f'<tr>'])
+
+                self.get_tabup()
+                for col in value1[0]:
+                    html = self.get_tab().join([html, f'<th>{col}</th>'])
+                html = self.get_tabdn().join([html, f'</tr>'])
+                html = self.get_tabdn().join([html, f'</thead>'])
+                self.get_tabdn()
+
+                for row in value1[1:]:
+                    html = self.get_tabup().join([html, f'<tr>'])
+                    self.get_tabup()
+                    for col in row:
+                        html = self.get_tab().join([html, f'<td>{col}</td>'])
+                    html = self.get_tabdn().join([html, f'</tr>'])
+                    self.get_tabdn()
+
+                html = self.get_tabdn().join([html, f'</table>'])
+            html = self.get_tabdn().join([html, f'</td>'])
+            html = self.get_tabdn().join([html, f'</tr>'])
+            html = self.get_tabdn().join([html, f'</table>'])
+        return html
+
     def load(self):
         for url in self.urls:
             r = requests.get(url)
@@ -65,9 +162,10 @@ class InfogramLoader(param.Parameterized):
             for infogram in infograms:
                 infogram_url, tables = self.extract_tables(infogram)
                 self.extract_data(infogram_url, tables)
+        return self.to_html()
 
 # urls = ["https://cawp.rutgers.edu/women-percentage-2020-candidates",
 #         "https://www.pcmag.com/news/meteor-lake-first-tests-intel-core-ultra-7-benched-for-cpu-graphics-and"]
 # ie = InfogramLoader(urls=urls)
-# ie.load()
-# print(ie.data)
+# html = ie.load()
+# print(html)
